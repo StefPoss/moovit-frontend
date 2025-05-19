efce4033e0a678eb21c75a0afd08604f1c48ecc7
Allaoui Sami <sami.allaoui@icloud.com>
2025-05-19 16:53:24 +0200

    First commit

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Cocomat: require("./assets/fonts/cocomat-pro-regular.ttf"),
+    Manrope: require("./assets/fonts/Manrope-VariableFont.ttf"),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "Cocomat", fontSize: 24 }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={{ fontFamily: "Manrope", fontSize: 24 }}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
