bfc4923d4e8190de6a90f3b930905ffae8813547
Allaoui Sami <sami.allaoui@icloud.com>
2025-05-19 19:50:31 +0200

    Update des fonts pour resolution des bugs Android

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { useFonts } from "expo-font";
+import { useFonts } from "expo-font"; // Import du module Expo pour l'usage des fonts
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  // Attention Android requiert le nom Exact des fonts - passage des fonts sur https://fontdrop.info/ pour avoir le nom reeal
   const [fontsLoaded] = useFonts({
-    Cocomat: require("./assets/fonts/cocomat-pro-regular.ttf"),
-    Manrope: require("./assets/fonts/Manrope-VariableFont.ttf"),
+    "CocomatPro-Regular": require("./assets/fonts/cocomat-pro-regular.ttf"), // Ajout des fonts (Modification du nom des polices pour compatibilité sur android)
+    "Manrope-ExtraLight": require("./assets/fonts/Manrope-ExtraLight.ttf"), // Ajout des fonts (Modification du nom des polices pour compatibilité sur android)
   });
 
   return (
     <View style={styles.container}>
-      <Text style={{ fontFamily: "Cocomat", fontSize: 24 }}>
+      {/* Test des polices  */}
+      <Text style={{ fontFamily: "CocomatPro-Regular", fontSize: 24 }}>
         Open up App.js to start working on your app!
       </Text>
-      <Text style={{ fontFamily: "Manrope", fontSize: 24 }}>
+      <Text style={{ fontFamily: "Manrope-ExtraLight", fontSize: 24 }}>
         Open up App.js to start working on your app!
       </Text>
       <StatusBar style="auto" />
