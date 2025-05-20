89538d3acc41e6df1c5f748c57acbfe94afbf082
Amel Bennaceur <amel_bennaceur@outlook.com>
2025-05-20 14:55:33 +0200

    frontendsplash

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import { useFonts } from "expo-font"; // Import du module Expo pour l'usage des 
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Splash from "./Screens/Auth/Splash";
-import Login from "./Screens/Auth/Login";
-import SignUp from "./Screens/Auth/Signup";
+import Splash from "./Screens/Auth/SplashScreen";
+import Login from "./Screens/Auth/LoginScreen";
+import SignUp from "./Screens/Auth/SignupScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +16,7 @@ export default function App() {
     "Manrope-ExtraLight": require("./assets/fonts/Manrope-ExtraLight.ttf"), // Ajout des fonts (Modification du nom des polices pour compatibilité sur android)
   });
 
-  if (!fontsLoaded) return null;
+  //if (!fontsLoaded) return null;
 
   return (
     <NavigationContainer>
@@ -38,32 +38,4 @@ const styles = StyleSheet.create({
   },
 });
 
-/*import { StatusBar } from 'expo-status-bar';
-import { useFonts } from 'expo-font';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import WelcomeScreen from './screens/WelcomeScreen';
-import SignUpScreen from './screens/SignUpScreen';
-import LoginScreen from './screens/LoginScreen';
 
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    'CocomatPro-Regular': require('./assets/fonts/cocomat-pro-regular.ttf'),
-    'Manrope-Extralight': require('./assets/fonts/Manrope-Extralight.ttf'),
-  });
-
-  if (!fontsLoaded) return null;
-
-  return (
-    <NavigationContainer>
-      <StatusBar style="light" />
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}*/
