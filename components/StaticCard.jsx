1fce0af6bbbcf4f5b3a55625f812eb81ce30dee4
Allaoui Sami <sami.allaoui@icloud.com>
2025-05-23 13:59:46 +0200

    te

--- components/StaticCard.jsx
+++ components/StaticCard.jsx
@@ -2,30 +2,18 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 function StaticCard({
-    title='',
-    width= "350", //long du boutton
-    height= "75", //haut du boutton
-    backgroundColor= "#000", //gris du figma
-    textColor="#000",
-}) {import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
-function StaticCard({
-    title='',
-    width= "350", //long du boutton
-    height= "75", //haut du boutton
-    backgroundColor= "#000", //gris du figma
-    textColor="#000",
+  title = "",
+  width = "350", //long du boutton
+  height = "75", //haut du boutton
+  backgroundColor = "#000", //gris du figma
+  textColor = "#000",
 }) {
-  
   return (
-    <View 
-    style={[styles.static, {width, height, backgroundColor }]}
-    >
+    <View style={[styles.static, { width, height, backgroundColor }]}>
       <Text style={[styles.staticText, { color: textColor }]}>{title}</Text>
     </View>
   );
-};
+}
 export default StaticCard;
 
 const styles = StyleSheet.create({
@@ -36,7 +24,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#000", //gris du figma
     margin: 5,
     padding: 5,
-    marginRight:27,
+    marginRight: 27,
   },
   staticText: {
     flex: 1,
@@ -45,37 +33,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginTop: 15,
     fontWeight: "700",
-    color:"white",
-  },
-});
-
-  return (
-    <View 
-    style={[styles.static, {width, height, backgroundColor }]}
-    >
-      <Text style={[styles.staticText, { color: textColor }]}>{title}</Text>
-    </View>
-  );
-};
-export default StaticCard;
-
-const styles = StyleSheet.create({
-  static: {
-    width: 350, //long du boutton
-    height: 75, //haut du boutton
-    borderRadius: 15, //arrondi des angles
-    backgroundColor: "#000", //gris du figma
-    margin: 5,
-    padding: 5,
-    marginRight:27,
-  },
-  staticText: {
-    flex: 1,
-    // justifyContent:"center",
-    // alignItems:"center",
-    textAlign: "center",
-    marginTop: 15,
-    fontWeight: "700",
-    color:"white",
+    color: "white",
   },
 });
