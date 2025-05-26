a3a1d7b00edf494600f92052c5bd32dd00de0b08
StefPoss <7715423+StefPoss@users.noreply.github.com>
2025-05-27 01:10:52 +0200

    Sécurisation temporaire de actiity is undefined

--- reducers/onBoardingSlice.js
+++ reducers/onBoardingSlice.js
@@ -1,22 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-  value: {},
-};
+  value: [],
+}
 
 export const onBoardingSlice = createSlice({
   name: "onBoarding",
   initialState,
   reducers: {
     addInfoToStore: (state, action) => {
-      state.value = action.payload;
-      console.log(state.value);
+      state.value = action.payload
+      console.log(state.value)
     },
     removeAllInfoToStore: (state, action) => {
-      state.value = initialState.value;
+      state.value = initialState.value
     },
   },
-});
+})
 
-export const { addInfoToStore, removeAllInfoToStore } = onBoardingSlice.actions;
-export default onBoardingSlice.reducer;
+export const { addInfoToStore, removeAllInfoToStore } = onBoardingSlice.actions
+export default onBoardingSlice.reducer
