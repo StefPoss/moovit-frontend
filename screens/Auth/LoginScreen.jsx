ea6ebefd13df484e5fa1df15f4408c7137ee08b4
stefposs <7715423+StefPoss@users.noreply.github.com>
2025-05-26 16:07:17 +0200

    dashboard > progress bar circulaire + profil

--- screens/Auth/LoginScreen.jsx
+++ screens/Auth/LoginScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 import {
   View,
   Text,
@@ -10,32 +10,29 @@ import {
   KeyboardAvoidingView,
   Keyboard,
   TouchableWithoutFeedback,
-} from "react-native";
-import { Ionicons, AntDesign, FontAwesome } from "@expo/vector-icons";
-import Button from "../../components/Buttons";
-import { addUserToStore } from "../../reducers/userSlice";
-import { useDispatch } from "react-redux";
-import { API_URL } from "@env";
-import { checkBody } from "../../modules/checkBody";
+} from "react-native"
+import { Ionicons, AntDesign, FontAwesome } from "@expo/vector-icons"
+import Button from "../../components/Buttons"
+import { addUserToStore } from "../../reducers/userSlice"
+import { useDispatch } from "react-redux"
+import { API_URL } from "@env"
+import { checkBody } from "../../modules/checkBody"
 //pour pouvoir pusher
 
-
-
 export default function LoginScreen({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [emailError, setEmailError] = useState("")
 
-  const [passwordVisible, setPasswordVisible] = useState(false);
-  const dispatch = useDispatch();
+  const [passwordVisible, setPasswordVisible] = useState(false)
+  const dispatch = useDispatch()
   const handleLogin = () => {
-
-    const requiredFields = ["email", "password"];
-    const body = { email, password };
+    const requiredFields = ["email", "password"]
+    const body = { email, password }
 
     if (!checkBody(body, requiredFields)) {
-      setEmailError("Tous les champs sont requis");
-      return;
+      setEmailError("Tous les champs sont requis")
+      return
     }
 
     // Envoi d'une requête POST à l'API backend pour la route /signin
@@ -50,36 +47,28 @@ export default function LoginScreen({ navigation }) {
     })
       .then((data) => data.json())
       .then((resultData) => {
-        console.log("-- ",resultData);
-        
+        console.log("-- ", resultData)
+
         // Si le login est réussi
         if (resultData.result && resultData.sport.length === 0) {
           //envoi vers le screen dashboard
-          navigation.navigate("onBoarding");
-          dispatch(addUserToStore({ token: resultData.token }));
+          navigation.navigate("onBoarding")
+          dispatch(addUserToStore({ token: resultData.token }))
         } else if (resultData.result && resultData.sport.length > 0) {
-          navigation.navigate("Dashboard");
-          dispatch(addUserToStore({ token: resultData.token }));
+          navigation.navigate("Dashboard")
+          dispatch(addUserToStore({ token: resultData.token }))
         } else {
-          alert(resultData.error);
+          alert(resultData.error)
         }
-      });
+      })
+  }
 
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      setEmailError("Email invalide");
-    } else {
-      setEmailError("");
-      console.log("Connexion réussie");
-      navigation.navigate("onBoarding"); // ou API
-    }
-  };
-
-
-    
-  
   return (
-    <TouchableWithoutFeedback onPress={()=>{Keyboard.dismiss()}}>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss()
+      }}
+    >
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : undefined}
         style={styles.container}
@@ -265,7 +254,7 @@ const styles = StyleSheet.create({
     textDecorationLine: "underline",
     fontWeight: "500",
   },
-});
+})
 
 /*const handleLogin = () => {
   const requiredFields = ['email', 'password'];
